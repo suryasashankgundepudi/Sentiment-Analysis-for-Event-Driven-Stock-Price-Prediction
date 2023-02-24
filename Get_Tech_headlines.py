fe57aad17637b9cb431469d95f15d6bfe1c62f13
Olivia <xyli9912@gmail.com>
2023-02-24 00:15:33 -0500

    Acquire data using GNews

--- Get_Tech_headlines.py
+++ Get_Tech_headlines.py
@@ -1,10 +1,9 @@
-from bs4 import BeautifulSoup as bs
 import pandas as pd
 import numpy as np
 
 # read in data and conduct basic data clean and rename columns
-df=pd.read_csv(r'nasdaq_screener_1675799625051.csv',usecols=["Symbol","Name","Last Sale","Market Cap","Industry"])
-df.columns = ["Symbol","Name","Last_Sale","Market_Cap","Industry"]
+df=pd.read_csv(r'nasdaq_screener_1675799625051.csv',usecols=["Symbol","Name",'Country',"Last Sale","Market Cap","Industry"])
+df.columns = ["Symbol","Name","Last_Sale","Market_Cap",'Country',"Industry"]
 df=df.dropna(how="any")
 
 # Acquire percent number and filter bottom 10 percent
@@ -15,35 +14,43 @@ tickerList = df[(df.Market_Cap > p)]
 # print(tickerList.head(10))
 
 #Technology
-Tech = tickerList[tickerList['Industry'].isin(['Computer Manufacturing', 'Computer Software: Prepackaged Software',
-                                               'Computer peripheral equipment','Computer Software: Programming Data Processing'
-                                               ,'Computer Communications Equipment','Internet and Information Services'])]
-
-
+Tech = tickerList[tickerList['Industry'].isin([ 'Computer Software: Prepackaged Software',
+                                               'Computer Software: Programming Data Processing'
+                                               ,'Computer Communications Equipment','Internet and Information Services',
+                                               'Retail: Computer Software & Peripheral Equipment'])]
+print(Tech.shape)
+Tech = Tech[Tech['Country'].isin(['United States'])]
 Tech = Tech.drop('Industry', axis=1)
+Tech = Tech.drop('Country',axis = 1)
+# print(Tech.head(10))
+# Tech.to_csv('my_dataframe.csv', index=False)
+print(Tech.shape)
+print(type(Tech.Name.tolist()))
+keyword = Tech.Name.tolist()
 # print(Tech.head(10))
 # Tech.to_csv('my_dataframe.csv', index=False)
 
-print(type(Tech.Symbol.tolist()))
-keyword = Tech.Symbol.tolist()
-from GoogleNews import GoogleNews
-import pandas as pd
+from gnews import GNews
+import time
+import requests.exceptions
 
-# create a GoogleNews object
-googlenews = GoogleNews(lang='en')
 data = []
-for symbol in keyword:
-    all_data = googlenews.search(symbol)
-    googlenews.setperiod('1d')
-    results = googlenews.result()
-# initialize
-    for result in results:
-        headline = result['title']
-        date = result['date']
-        data.append([headline, date, symbol])
+try:
+    googlenews = GNews(language='en', country='US', period='3m')
+    for symbol in keyword:
+        print('1') # check progress
+        time.sleep(3)
 
-df = pd.DataFrame(data, columns=['Headline', 'Publish Date','Company'])
+        for result in googlenews.get_news(symbol):
+            headline = result['title']
+            date = result['published date']
+            data.append([headline, date, symbol])
+    import pandas as pd
+    df = pd.DataFrame(data, columns=['Headline', 'Publish Date', 'company'])
 
-# export the dataframe to a CSV file
-df.to_csv('news_headlines.csv', index=False)
-print(df)
+    # export the dataframe to a CSV file
+    df.to_csv('test.csv', index=False)
+    print(df)
+
+except requests.exceptions.RequestException as e:
+    print(f"Error acquiring news: {e}")
