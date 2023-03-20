6643f273b04e92d4ba859580c36d943b60a138a3
GV-Gundepudi <svgundep@ncsu.edu>
2023-03-20 09:04:13 -0400

    Refactoring files for us to run

--- data_preprocessing/get_prices.py
+++ data_preprocessing/get_prices.py
@@ -2,7 +2,7 @@ import yfinance as yf
 from datetime import datetime
 import os
 
-os.chdir("D:/PYTHON3/Sentiment-Analysis-for-Event-Driven-Stock-Price-Prediction/data preprocessing/")
+os.chdir("/data preprocessing/")
 
 today = datetime.today().date()
 
