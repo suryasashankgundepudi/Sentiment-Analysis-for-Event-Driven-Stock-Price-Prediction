0db837e5f8be6d9c0ff032a0537e1474a40606c3
GV-Gundepudi <svgundep@ncsu.edu>
2023-03-20 09:25:52 -0400

    Final Refactoring

--- data_preprocessing/run_all_data_files.py
+++ data_preprocessing/run_all_data_files.py
@@ -1,7 +1,19 @@
-from collect_reuters import *
-from get_prices import *
-from combine_head_stock import *
-ticker = "META"
-run_headlines(ticker)
-download_prices(ticker)
-combine_headlines(ticker)
+from data_preprocessing.collect_reuters import *
+from data_preprocessing.get_prices import *
+from data_preprocessing.combine_head_stock import *
+import argparse
+import os
+
+
+parser = argparse.ArgumentParser(description='Sentiment analyzer')
+
+parser.add_argument('-a', action="store_true", default=False)
+
+parser.add_argument('--ticker', type=str, default= "META", help='Path to the text file.')
+
+args = parser.parse_args()
+
+print(args.ticker)
+#run_headlines(args.ticker)
+#download_prices(ticker)
+#combine_headlines(ticker)
